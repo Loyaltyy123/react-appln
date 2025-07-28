fdbf5c00750b7c7e245e37b3911434db7fc6633b
sathvik123 <sri1678622@gmail.com>
2025-07-28 15:48:05 +0530

    deletion completed

--- src/components/DeleteComp.jsx
+++ src/components/DeleteComp.jsx
@@ -1,4 +1,16 @@
+import axios from "axios"
+import { useEffect } from "react"
 const DeleteComp = ()=>{
+    const deleteUser=async ()=>{
+        const res=await axios.delete("https://reqres.in/api/users",{headers:{
+                "x-api-key": "reqres-free-v1",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`}
+            })
+            console.log(res)
+    }
+    useEffect(()=>{
+        deleteUser();
+    },[])
     return(
         <>
             <p>DeleteComp Soon...!</p>
