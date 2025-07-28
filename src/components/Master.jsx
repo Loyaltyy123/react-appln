cd16f1f608ee0fb9e2ba31da47b2cb3a114edcd8
Loyalty123 <info@careerit.in>
2025-07-28 16:02:21 +0530

    code

--- src/components/Master.jsx
+++ src/components/Master.jsx
@@ -1,25 +1,28 @@
+import { Suspense,lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./Login";
-import Dashboard from "./Dashboard";
-import GetComp from "./GetComp";
-import PostComp from "./PostComp";
-import PutComp from "./PutComp";
-import DeleteComp from "./DeleteComp";
-import Error from "./Error";
+const Login = lazy(()=> import("./Login"));
+const Dashboard = lazy(()=> import("./Dashboard"));
+const GetComp = lazy(()=> import("./GetComp"));
+const PostComp = lazy(()=> import("./PostComp"));
+const PutComp = lazy(()=> import("./PutComp"));
+const DeleteComp = lazy(()=> import("./DeleteComp"));
+const Error = lazy(()=> import("./Error"));
 const Master = ()=>{
     return(
         <>
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Login></Login>}></Route>
-                    <Route path="/dashboard" element={<Dashboard></Dashboard>}>
-                        <Route path="dashboard/get" element={<GetComp></GetComp>}></Route>
-                        <Route path="dashboard/post" element={<PostComp></PostComp>}></Route>
-                        <Route path="dashboard/put" element={<PutComp></PutComp>}></Route>
-                        <Route path="dashboard/delete" element={<DeleteComp></DeleteComp>}></Route>
-                    </Route>
-                    <Route path="/error" element={<Error></Error>}></Route>
-                </Routes>
+                <Suspense fallback={<div>Loading....!</div>}>
+                    <Routes>
+                        <Route path="/" element={<Login></Login>}></Route>
+                        <Route path="/dashboard" element={<Dashboard></Dashboard>}>
+                            <Route path="dashboard/get" element={<GetComp></GetComp>}></Route>
+                            <Route path="dashboard/post" element={<PostComp></PostComp>}></Route>
+                            <Route path="dashboard/put" element={<PutComp></PutComp>}></Route>
+                            <Route path="dashboard/delete" element={<DeleteComp></DeleteComp>}></Route>
+                        </Route>
+                        <Route path="/error" element={<Error></Error>}></Route>
+                    </Routes>
+                </Suspense>
             </BrowserRouter>
         </>
     )
